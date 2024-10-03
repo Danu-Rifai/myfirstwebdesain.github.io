2bafedb50933a05b15f0fd58a15aa61c90ac1819
Danurifai <rifaidanu12@gmail.com>
2024-10-03 15:57:06 +0700

    Delete JS/course.js

--- JS/course.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to add 'scrolled' class on scroll
-window.addEventListener('scroll', function() {
-    var nav = document.getElementById('nav'); // Ambil elemen navbar berdasarkan ID
-    if (window.scrollY > 50) { // Jika scroll lebih dari 50px
-        nav.classList.add('scrolled'); // Tambahkan class 'scrolled'
-    } else {
-        nav.classList.remove('scrolled'); // Hapus class 'scrolled' jika kembali ke posisi atas
-    }
-});
